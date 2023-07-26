4a0337610ee25e4903d1733bedbeb3f91c0cade4
mishadcf <121932224+mishadcf@users.noreply.github.com>
2023-07-26 09:09:06 +0100

    Update milestone_4.py

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,24 +1,4 @@
-# Create a new script called milestone_4.py. This file will contain the code for the third milestone.
 
-# Define the init method of the Hangman class.
-
-# Step 1. Create a class called Hangman.
-
-# Step 2. Inside the class, create an init method to initialise the attributes of the class. Pass in word_list and num_lives as parameters. Make num_lives a default parameter and set the value to 5.
-
-# Step 3. Initialise the following attributes:
-
-# word: The word to be guessed, picked randomly from the word_list. Remember to import the random module into your script.
-
-# word_guessed: list - A list of the letters of the word, with for each letter not yet guessed. For example, if the word is 'apple', the word_guessed list would be ['', '', '', '', '']. If the player guesses 'a', the list would be ['a', '', '', '', ''].
-
-# num_letters: int - The number of UNIQUE letters in the word that have not been guessed yet.
-
-# num_lives: int - The number of lives the player has at the start of the game.
-
-# word_list: list - A list of words.
-
-# list_of_guesses: list - A list of the guesses that have already been tried. Set this to an empty list initially.
 
 
 from milestone_2 import (
