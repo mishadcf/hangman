5b321405315466e9e3a04fc59576fd4da7db7ff3
mishadcf <misha.freidin@gmail.com>
2023-07-27 15:56:50 +0100

    Updates README to reflect milestone_4 updates

--- README.md
+++ README.md
@@ -19,4 +19,13 @@ This is an implementation of the Hangman game, where the computer thinks of a wo
 - ask_for_input prompts the user to input their guess (a single alphanumeric character), checks validity of inputs- then uses that validated input as a parameter for the check_guess() function
 - check_guess checks whether the guessed letter is in the secret word and outputs messages to let the user know whether their guess was correct
 
+# Milestone 4:
+- Defined a Hangman class with init method taking in a word_list parameter and num_lives (number of lives) with value 5 as a default
+- Defined the following attributes of hangman instances: word, word_guessed (starts with blanks and updates with correct letter guesses), num_letters (number of unique letters that have not been guessed yet), num_lives, word_list (list of words), list_of_guesses (list of unique guesses that have been tried)
+- Defined instance methods: ask_for_input, check_guess 
+- ask_for_input checks validity of user guesses, then checks whether the input letter has already been guessed. If both of these conditions aren't met, then the check_guess method is run
+- check_guess is defined to check if the validated guess is in the secret word. If it is, then a message to that effect is printed and the letter takes the position(s) of the underscore ('_') in the word_guessed attribute of that instance.
+- if the guess is not in word, a message to that effect is printed and the player loses a life (self.num_lives reduces by 1)
+
+
 
--- milestone_4.py
+++ milestone_4.py
@@ -1,17 +1,51 @@
 from milestone_2 import (
     word,
-)  # check this import is correct at the end - milestone_2.py should take care of the random word choice from word_list
+)
+
+# from milestone_3 import check_guess, ask_for_input
 import string
 
+# check this import is correct at the end - milestone_2.py should take care of the random word choice from word_list, milestone_3.py already has a check_guess function (DRY)
 
+
+# Remember, this takes in word from milestone 2 - can adapt the below to arbitrary word list if needed
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = word  # initalise attributes inside of the init function? word here accesses word from import?
         self.word_guessed = [
-            None for i in range(len(word))
-        ]  # attempts to initialise blanks output to user of the same length as the secret word
+            "_" for i in range(len(word))
+        ]  #  initialise blanks output to user of the same length as the secret word
         self.num_letters = {
             letter for letter in string.ascii_letters
         }  # initalises all asci_letters as a set, before user has made their choice. After, these will be removed/popped. What if this weren't a set?
+        self.list_of_guesses = []  # keeps track of all unique guesses
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in word:
+            print(f"Good guess! {guess} is in the word.")
+            for idx, letter in enumerate(
+                word
+            ):  # takes care of the case where the same letter exists in the word in different locations
+                if guess == letter:
+                    self.word_guessed[idx] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ")
+            if not (guess.isalpha() and len(guess) == 1):  # checks input is valid
+                print("Invalid letter. Please, enter a single alphabetical character.")
+
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
